352c8342b56286044ef6a8ab379ae963d525df4e
s1270146 <[s1270146@gmail.com]>
2021-10-22 12:22:27 +0900

    Fixed ex04

--- ex04/Date.cpp
+++ ex04/Date.cpp
@@ -28,17 +28,140 @@ int Date::get_day() const
     return day;
 }
 
-void Date::inc_day()
+void Date::inc_month()
 {
+    int flag;
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                flag = 1;
+            }
+            else
+            {
+                flag = 0;
+            }
+        }
+        else
+        {
+            flag = 1;
+        }
+    }
+    else
+    {
+        flag = 0;
+    }
+    if (month == 3 || month == 5 || month == 8 || month == 10)
+    {
+        if (day == 31)
+            day = 30;
+    }
+    else if (month == 1)
+    {
+        if(flag == 0)
+        {
+            if (day >= 29 && day <= 31)
+            day = 28;
+        }
+        else if(flag == 1)
+        {
+            if(day >= 30 && day <= 31)
+            day = 29;
+        }
+    }
+    
+    if (month == 12)
+    {
+        month = 0;
+        inc_year();
+    }
     month++;
 }
 
-void Date::inc_month()
+void Date::inc_day()
 {
+    int flag;
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                flag = 1;
+            }
+            else
+            {
+                flag = 0;
+            }
+        }
+        else
+        {
+            flag = 1;
+        }
+    }
+    else
+    {
+        flag = 0;
+    }
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+    {
+        if (day == 31)
+            day = 0;
+    }
+    else if (month == 2)
+    {
+        if(flag == 0)
+        {
+            if (day == 28)
+            day = 0;
+        }
+        else if(flag == 1)
+        {
+            if(day == 29)
+            day = 0;
+        }
+    }
+    else
+    {
+        if (day == 30)
+            day = 0;
+    }
+    if (day == 0)
+        inc_month();
     day++;
 }
 
 void Date::inc_year()
 {
+    int flag;
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                flag = 1;
+            }
+            else
+            {
+                flag = 0;
+            }
+        }
+        else
+        {
+            flag = 1;
+        }
+    }
+    else
+    {
+        flag = 0;
+    }
+    if(flag == 1)
+    {
+        if(day==29)day = 28;
+    }
+
     year++;
 }
--- ex04/test_date.cpp
+++ ex04/test_date.cpp
@@ -1,15 +1,54 @@
 #include "Date.h"
+#include <iostream>
 
-int main(void) {
+using namespace std;
+
+int main(void)
+{
   Date d;
-  d.set(10, 1, 1998);
-  d.print();
+  int month, day, year, com;
+  cout << "Input the number of month, day and year -> ";
+  cin >> month >> day >> year;
+  d.set(month, day, year);
 
-  d.inc_day();
-  d.inc_month(); 
-  d.print(); 
+  cout << endl;
+  cout << "---------------------------------------" << endl;
+  cout << " The command Number " << endl;
+  cout << " 1 : Increase the number of month" << endl;
+  cout << " 2 : Increase the number of day" << endl;
+  cout << " 3 : Increase the number of year" << endl;
+  cout << " 4 : Help" << endl;
+  cout << " 5 : Finish" << endl;
+  cout << "---------------------------------------" << endl;
 
-  d.inc_year();
-  d.print(); 
+  while (1)
+  {
+    cout << endl;
+    d.print();
+    cout << endl
+         << "Input the command number -> ";
+
+    cin >> com;
+
+    if (com == 1)
+      d.inc_month();
+    if (com == 2)
+      d.inc_day();
+    if (com == 3)
+      d.inc_year();
+    if (com == 4)
+    {
+      cout << endl;
+      cout << "---------------------------------------" << endl;
+      cout << " The command Number " << endl;
+      cout << " 1 : Increase the number of month" << endl;
+      cout << " 2 : Increase the number of day" << endl;
+      cout << " 3 : Increase the number of year" << endl;
+      cout << " 4 : Help" << endl;
+      cout << " 5 : Finish" << endl;
+      cout << "---------------------------------------" << endl;
+    }
+    if (com == 5)
+      break;
+  }
 }
-
